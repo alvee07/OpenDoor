b976de8d07dbb1d34244c69302baaad12bb86dd9
alvee2020 <alvee@ualberta.ca>
2020-01-09 15:28:01 -0700

    Manifest- screen lock drawable- opendoorlogo image added color- color added based on Open door logo string- few strings added based on needs of activity style- no action bar changed .idea- google java format settings added

---
+++ app/src/main/java/com/example/opendoorapp/ConfirmationActivity.java
@@ -0,0 +1,19 @@
+package com.example.opendoorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConfirmationActivity extends AppCompatActivity {
+  
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_confirmation);
+  }
+  
+  
+  
+  
+  
+}
