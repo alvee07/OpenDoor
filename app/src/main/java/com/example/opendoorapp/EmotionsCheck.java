fe0835dd6f3a01127f0bd7e79f8184254a3fb453
Benjamin <bwilson1@ualberta.ca>
2020-01-09 16:59:48 -0700

    added EmotionsCheck

---
+++ app/src/main/java/com/example/opendoorapp/EmotionsCheck.java
@@ -0,0 +1,14 @@
+package com.example.opendoorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmotionsCheck extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_emotions_check);
+  }
+}
