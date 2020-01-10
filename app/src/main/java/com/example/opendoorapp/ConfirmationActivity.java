646d475ba6a1c16fb84765830447648226e5a2af
alvee2020 <alvee@ualberta.ca>
2020-01-10 09:05:47 -0700

    ConfirmationActivity- Thanks User, Confirmation message strings separated, functions added Assuming previous activity sends data, stores data in the local file, will send message from this page. Resources folder updated,

--- app/src/main/java/com/example/opendoorapp/ConfirmationActivity.java
+++ app/src/main/java/com/example/opendoorapp/ConfirmationActivity.java
@@ -22,20 +22,25 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class ConfirmationActivity extends AppCompatActivity {
+  
   /**
+   * Variable list
+   *
+   * thanksUser - String value
+   * confirmationMessage - String value
    * userName - String value - Store user name
-   * */
-  String userName;
-  
-  
-  TextView confirmationMessage01, confirmationMessage02;
-  
+   * services - String value - Store what type of services user chose
+   * workers - String value - Store who they chose to talk
+   * emotions - String value - Store how they are feeling today
+   */
+  TextView thanksUser, confirmationMessage;
+  String userName, services, workers, emotions;
   
   /**
    * When the program starts.
    * 1) Set up 'userName' variable to given name from 'Services/Emotion Activity' class.
-   * 2) Shows the confirmation string with name.
-   * 3) This activity stays active for 10000000000000000 seconds and go back to 'Main Activity' class.
+   * 2) Shows the confirmation message string on the screen with userName.
+   * 3) This activity stays active for 10000000000000000 seconds and goes back to 'Main Activity' class.
    *
    * @param savedInstanceState
    */
@@ -44,28 +49,12 @@ public class ConfirmationActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_confirmation);
     
-    // getting userName from
-    userName = getIntent().getStringExtra("userName");
-    userName = "AAB";
     
-    confirmationMessage01 = findViewById(R.id.confirmationMessage01);
-    confirmationMessage01.setText(buildConfirmationMessage(userName));
   
-    confirmationMessage02 = findViewById(R.id.confirmationMessage02);
-  
-    confirmationMessage02.setText(R.string.confirmationMessage02);
-
-    // Start Main Activity in 5 seconds
-
-    new Handler().postDelayed(new Runnable() {
-      @Override
-      public void run() {
-        final Intent goBackToMainActivity = new Intent(ConfirmationActivity.this, MainActivity.class);
-        ConfirmationActivity.this.startActivity(goBackToMainActivity);
-        ConfirmationActivity.this.finish();
-      }
-    }, 100000);
-  
+    setThanksUser();
+    setConfirmationMessage();
+    
+    startMainActivity();
   
   
     Toast.makeText(getApplicationContext(),"this is confirmation page",Toast.LENGTH_SHORT).show();
@@ -75,16 +64,75 @@ public class ConfirmationActivity extends AppCompatActivity {
   
   
   
-  public String buildConfirmationMessage(String userName){
-    String confirmationMessage01 = getString(R.string.confirmationMessage01);
-    
+  
+  /**
+   * Sets userName, services, workers, emotions from previous activity - (Services/Emotion Activity)
+   */
+  private void setUsercredentials(){
+    if (getIntent().hasExtra("USERNAME") &&
+            getIntent().hasExtra("SERVICES") &&
+            getIntent().hasExtra("WORKERS") &&
+            getIntent().hasExtra("EMOTIONS")){
+  
+      userName = getIntent().getStringExtra("USERNAME");
+      services = getIntent().getStringExtra("SERVICES");
+      workers = getIntent().getStringExtra("WORKERS");
+      emotions = getIntent().getStringExtra("EMOTIONS");
+
+    } // if
+  //for testing
+    userName = "AAB";
+  }
+  
+  
+  
+  /**
+   * Gets thanksUser textView from XML
+   * Sets thanks user message with userName variable on the screen
+   */
+  private void setThanksUser(){
+    setUsercredentials();
+    thanksUser = findViewById(R.id.thanksUser);
+    thanksUser.setText(buildThanksUserMessage(userName));
+  }
+  
+  /**
+   * Gets confirmationMessage textView from XML
+   * Sets confirmationMessage message on the screen
+   */
+  private void setConfirmationMessage(){
+    confirmationMessage = findViewById(R.id.confirmationMessage);
+    confirmationMessage.setText(R.string.confirmationMessage);
+  }
+  
+  /**
+   * Takes the userName, adds userName to resource string with !
+   * @param userName - String value
+   * @return message - String value
+   */
+  public String buildThanksUserMessage(String userName){
+    String confirmationMessage01 = getString(R.string.thanksUser);
     StringBuilder confirmationMessage = new StringBuilder(confirmationMessage01);
-    
     confirmationMessage.append(userName);
     confirmationMessage.append(" !");
-    
-    return confirmationMessage.toString();
+    String message = confirmationMessage.toString();
+    return message;
   }
+  
+  /**
+   * Starts MainActivity class in 10000000 seconds
+   */
+  public void startMainActivity(){
+  
+    new Handler().postDelayed(new Runnable() {
+      @Override
+      public void run() {
+        final Intent goBackToMainActivity = new Intent(ConfirmationActivity.this, MainActivity.class);
+        ConfirmationActivity.this.startActivity(goBackToMainActivity);
+        ConfirmationActivity.this.finish();
+      }
+    }, 100000);
+  } // startMainActivity
   
   
 } // ConfirmationActivity
