255f2a2d419903af345b44deb528b1b6ceea975c
alvee2020 <alvee@ualberta.ca>
2020-01-10 16:47:09 -0700

    Merge remote-tracking branch 'remotes/origin/Alvee_01' into Developing
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/opendoorapp/MainActivity.java
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/opendoorapp/NameActivity.java
+++ app/src/main/java/com/example/opendoorapp/NameActivity.java
@@ -9,6 +9,6 @@ public class NameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_name2);
+        setContentView(R.layout.activity_name);
     }
 }
