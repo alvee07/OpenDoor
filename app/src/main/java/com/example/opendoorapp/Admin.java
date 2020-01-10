7a337536da3f0a78b596dd488223deaa37910c87
awwtaylor <56772048+awwtaylor@users.noreply.github.com>
2020-01-10 14:14:44 -0700

    Added small timer method too move to main activity

--- app/src/main/java/com/example/opendoorapp/Admin.java
+++ app/src/main/java/com/example/opendoorapp/Admin.java
@@ -2,13 +2,29 @@ package com.example.opendoorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 public class Admin extends AppCompatActivity {
 
+    Timer timeout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+        timeout = new Timer();
+        timeout.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent homePage = new Intent (getApplicationContext(), MainActivity.class);
+                startActivity(homePage);
+                finish();
+            }
+        }, 15000);
     }
 }
