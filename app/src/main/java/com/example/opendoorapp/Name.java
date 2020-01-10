73baf062b96563e9e7be3b394293fc3783ecbb56
arnoldgihozo <gihozo@ualberta.ca>
2020-01-09 18:46:31 -0700

    created activity and java class of Name

---
+++ app/src/main/java/com/example/opendoorapp/Name.java
@@ -0,0 +1,12 @@
+package com.example.opendoorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Name extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_name);
+    }
+}
