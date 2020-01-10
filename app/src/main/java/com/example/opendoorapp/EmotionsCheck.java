bd60f4ea44903f5604d4f9c073ff1d6cf589cd64
Benjamin <bwilson1@ualberta.ca>
2020-01-10 11:51:35 -0700

    added onclick for all buttons

--- app/src/main/java/com/example/opendoorapp/EmotionsCheck.java
+++ app/src/main/java/com/example/opendoorapp/EmotionsCheck.java
@@ -11,6 +11,15 @@ public class EmotionsCheck extends AppCompatActivity {
   private String selectedEmotion;
 
 
+  Button happyButton;
+  Button contentButton;
+  Button neutralButton;
+  Button upsetButton;
+  Button sadButton;
+  Button angryButton;
+  Button submitButton;
+
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -18,22 +27,90 @@ public class EmotionsCheck extends AppCompatActivity {
 
     selectedEmotion = "null";
 
-    Button happyButton = findViewById(R.id.happyButton);
-    Button contentButton = findViewById(R.id.contentButton);
-    Button neutralButton = findViewById(R.id.neutralButton);
-    Button upsetButton = findViewById(R.id.upsetButton);
-    Button sadButton = findViewById(R.id.sadButton);
-    Button angryButton = findViewById(R.id.angryButton);
-    Button submitButton = findViewById(R.id.submitButton);
+    happyButton = findViewById(R.id.happyButton);
+    contentButton = findViewById(R.id.contentButton);
+    neutralButton = findViewById(R.id.neutralButton);
+    upsetButton = findViewById(R.id.upsetButton);
+    sadButton = findViewById(R.id.sadButton);
+    angryButton = findViewById(R.id.angryButton);
+    submitButton = findViewById(R.id.submitButton);
 
 
     happyButton.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
 
+        selectedEmotion = "happy";
+
+        happyButton.setPressed(true);
+        checkOtherButtons(selectedEmotion);
       }
     });
-  }
+
+    contentButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+
+        selectedEmotion = "content";
+
+        contentButton.setPressed(true);
+        checkOtherButtons(selectedEmotion);
+      }
+    });
+
+    neutralButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+
+        selectedEmotion = "neutral";
+
+        neutralButton.setPressed(true);
+        checkOtherButtons(selectedEmotion);
+      }
+    });
+
+    upsetButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+
+        selectedEmotion = "upset";
+
+        upsetButton.setPressed(true);
+        checkOtherButtons(selectedEmotion);
+      }
+    });
+
+    sadButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+
+        selectedEmotion = "sad";
+
+        sadButton.setPressed(true);
+        checkOtherButtons(selectedEmotion);
+      }
+    });
+
+    angryButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+
+        selectedEmotion = "angry";
+
+        angryButton.setPressed(true);
+        checkOtherButtons(selectedEmotion);
+      }
+    });
+
+    submitButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+
+
+      }
+    });
+
+  }//onCreate
 
 
 }
