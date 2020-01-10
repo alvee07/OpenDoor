00b735dddfb796c09582f7cf84472afb0759b34a
awwtaylor <56772048+awwtaylor@users.noreply.github.com>
2020-01-10 16:13:12 -0700

    Fixed Android Manifest

--- app/src/main/java/com/example/opendoorapp/MainActivity.java
+++ app/src/main/java/com/example/opendoorapp/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.opendoorapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,11 +16,16 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
+    String password = "hello";
+    EditText passwordInput = new EditText(MainActivity.this);
     Button admin = (Button) findViewById(R.id.admin);
     admin.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
         Intent goToAdmin = new Intent(getApplicationContext(), Admin.class);
+        AlertDialog.Builder password = new AlertDialog.Builder(getApplicationContext());
+        password.setTitle("Admin ");
+        password.setMessage("Please Enter Admin Password");
         startActivity(goToAdmin);
       }
     });
