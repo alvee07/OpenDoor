a5baf5fe71854c544115a774a33a495b3da24d39
arnoldgihozo <gihozo@ualberta.ca>
2020-01-10 16:16:04 -0700

    Created a new ServicesActivity and NameActivity. Updated the services

--- app/src/main/java/com/example/opendoorapp/ServicesActivity.java
+++ app/src/main/java/com/example/opendoorapp/ServicesActivity.java
@@ -6,27 +6,36 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-public class Services extends AppCompatActivity {
+public class ServicesActivity extends AppCompatActivity {
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.services);
+        setContentView(R.layout.activity_services);
 
         // Services spinner!
         Spinner services = (Spinner) findViewById(R.id.servicesSpinner);
 
         //container that hold the values and integrate them with the spinner
-        ArrayAdapter<String> myadapter = new ArrayAdapter<String>(Services.this,
+        ArrayAdapter<String> myadapter = new ArrayAdapter<String>(ServicesActivity.this,
                 android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.services));
 
         //specifiy that it is a drop down list
         myadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         services.setAdapter(myadapter);
 
+
+
+        // Drop down menu for the names
+
         Spinner names = (Spinner) findViewById(R.id.namesSpinner);
+
+        // Container that hold the values and integrate them with the spinner
+        ArrayAdapter<String> namesAdapter = new ArrayAdapter<String>(ServicesActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.names));
+
+        myadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        names.setAdapter(namesAdapter);
+
     }
-
-
-
-
-
 }
