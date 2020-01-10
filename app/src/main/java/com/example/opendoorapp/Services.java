9477566b900cac9b7874ceb3da1994b932e43937
arnoldgihozo <gihozo@ualberta.ca>
2020-01-09 19:39:30 -0700

    Drop down menu for services (the services are stored in a string file)

--- app/src/main/java/com/example/opendoorapp/Services.java
+++ app/src/main/java/com/example/opendoorapp/Services.java
@@ -1,4 +1,29 @@
 package com.example.opendoorapp;
 
-public class Services {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class Services extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.services);
+
+        Spinner services = (Spinner) findViewById(R.id.servicesSpinner);
+
+        //container that hold the values and integrate them with the spinner
+        ArrayAdapter<String> myadapter = new ArrayAdapter<String>(Services.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.services));
+
+        //specifiy that it is a drop down list
+        myadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        services.setAdapter(myadapter);
+    }
+
+
+
+
+
 }
