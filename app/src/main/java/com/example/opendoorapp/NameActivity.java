7e2cadcc07bbce323ef11a9422fe3c4ff65b2ee2
arnoldgihozo <gihozo@ualberta.ca>
2020-01-11 13:57:24 -0700

    Added an onclick to get the user's name stored inside the User object

--- app/src/main/java/com/example/opendoorapp/NameActivity.java
+++ app/src/main/java/com/example/opendoorapp/NameActivity.java
@@ -3,12 +3,33 @@ package com.example.opendoorapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+
 
 public class NameActivity extends AppCompatActivity {
+
+    private User currentUser;
+    private String name;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_name);
+
+        Button submitBtn = (Button) findViewById(R.id.whatINeed);
+        final EditText nameTxt = (EditText) findViewById(R.id.nameTextView);
+        submitBtn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                name = nameTxt.getText().toString();
+                //currentUser.setName(name);
+            }
+        });
+
     }
-}
+    }
+
