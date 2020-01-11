4daa7cbcd53e2c517354adb93246ae2447d5b4ba
awwtaylor <56772048+awwtaylor@users.noreply.github.com>
2020-01-11 15:30:46 -0700

    Added Rough Timer for Inactivity, and added back button to send view of admin page to main activity

--- app/src/main/java/com/example/opendoorapp/Admin.java
+++ app/src/main/java/com/example/opendoorapp/Admin.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -17,14 +20,39 @@ public class Admin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
 
+        ImageButton backButton = (ImageButton) findViewById(R.id.backTo);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToHome();
+            }
+        });
+//        startTimer();
+    }
+
+    public void onUserInteraction(){
+        super.onUserInteraction();
+        stopTimer();
+        startTimer();
+    }
+
+    private void startTimer() {
         timeout = new Timer();
         timeout.schedule(new TimerTask() {
             @Override
             public void run() {
-                Intent homePage = new Intent (getApplicationContext(), MainActivity.class);
-                startActivity(homePage);
-                finish();
+                goToHome();
             }
         }, 15000);
+
+    }
+
+    private void stopTimer() {
+        timeout.cancel();
+    }
+
+    private void goToHome(){
+        Intent homePage = new Intent (getApplicationContext(), MainActivity.class);
+        startActivity(homePage);
     }
 }
