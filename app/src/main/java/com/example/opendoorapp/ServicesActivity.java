d923d13d7f297cadb2ae5b37911566a24c266678
arnoldgihozo <gihozo@ualberta.ca>
2020-01-14 10:51:45 -0700

    Added comments and refactored code

--- app/src/main/java/com/example/opendoorapp/ServicesActivity.java
+++ app/src/main/java/com/example/opendoorapp/ServicesActivity.java
@@ -5,7 +5,9 @@
  *
  *
  * This class will give the opportunity to the user to choose between either a service
- * or a worker.
+ * or a worker. If a specific services is taken, it will then lead the user to choose
+ * their emotion.
+ *
  */
 
 package com.example.opendoorapp;
@@ -29,7 +31,7 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 public class ServicesActivity extends AppCompatActivity implements OnItemSelectedListener {
-  
+
   private String selectedServices;
   private String selectedWorker;
   private User currentUser;
@@ -37,90 +39,158 @@ public class ServicesActivity extends AppCompatActivity implements OnItemSelecte
   private Boolean isSelectedWorker;
   private Spinner services;
   private Spinner worker;
-  
   private GestureDetectorCompat mDetector;
-  
-  
+
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_services);
-    
-    // Alvee is working onTouchEvent for timer
-    //mDetector = new GestureDetectorCompat(this, new MyGestureListener());
-    
-    // Services
-    // ===========================================================================
-    services = (Spinner) findViewById(R.id.servicesSpinner);
+
+    servicesSpinner();
+    workerSpinner();
+
+    services.setOnItemSelectedListener(this);
+    worker.setOnItemSelectedListener(this);
+
+
+  } // end of onCreate
+
+
+  /**
+   * Sets up the services spinner (with the items from the string.xml file) and sets up
+   * the drop down menu
+   *
+   * by Arnold
+   *
+   * Initial setup of this has been inspired from this source
+   * https://developer.android.com/guide/topics/ui/controls/spinner.
+   * Accessed on January 11, 2020
+   */
+  public void servicesSpinner(){
+    services =  findViewById(R.id.servicesSpinner);
     //container that hold the values and integrate them with the spinner
     ArrayAdapter<String> servicesAdapter = new ArrayAdapter<String>(ServicesActivity.this,
             android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.services));
-    
-    //specifiy that it is a drop down list
-    //services.setBackgroundColor(R.color.openDoorBackGroundColor);
+
+    //Drop down list of services stored in .xml file
     servicesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
     services.setAdapter(servicesAdapter);
-    //=============================================================================
-    
+  } // end of servicesSpinner
+
+  /**
+   * Sets up the worker spinner (with the items from the string.xml file) and sets up
+   * the drop down menu
+   *
+   * by Arnold
+   *
+   * Initial setup of this has been inspired from this source
+   * https://developer.android.com/guide/topics/ui/controls/spinner.
+   * Accessed on January 11, 2020
+   */
+  public void workerSpinner(){
     //Workers
     //==============================================================================
-    worker =(Spinner) findViewById(R.id.workerSpinner);
+    worker = findViewById(R.id.workerSpinner);
     // Container that hold the values and integrate them with the spinner
     ArrayAdapter<String> workerAdapter = new ArrayAdapter<String>(ServicesActivity.this,
             android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.names));
-    
-    servicesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+    workerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
     worker.setAdapter(workerAdapter);
-    //======================================================================================
-    services.setOnItemSelectedListener(this);
-    worker.setOnItemSelectedListener(this);
-    
-    //startMainActivity();
-  } // end of onCreate
-  
+
+  }// end of workerSpinner
+
   /**
+   * Checks the selections of the user. If user selects services, it disables the staff
+   * lists and vice versa.
    *
-   * @param parent
-   * @param v
-   * @param position
-   * @param id
+   * @param parent -- adapter view of where the selection has happened
+   * @param view -- the view that was selected
+   * @param position -- position of the view
+   * @param id -- the id of the item selected
+   *
+   * To excecute this section, I looked over the code on StackOver flow on Saturday January 11, 2020
+   * https://stackoverflow.com/questions/4476379/spinner-switch-case-problem
+   * Changes have been made to fit this project and refactoring the code
    *
    * by Arnold
    */
-  public void onItemSelected (AdapterView<?> parent, View v, int position, long id) {
-    
-    services.setPopupBackgroundResource(R.color.servicesSpinnerBackground);
-    worker.setPopupBackgroundResource(R.color.servicesSpinnerBackground);
-    
+  public void onItemSelected (AdapterView<?> parent, View view, int position, long id) {
+
+    setBackgroundColor();
+
     if (parent.getItemAtPosition(position).equals("-- Choose an option --")) {
-      services.setEnabled(true);
-      worker.setEnabled(true);
-      
+      enableSpinners();
+
     } else {
-      
+
       switch (parent.getId()) {
         case R.id.servicesSpinner:
-          //Toast.makeText(parent.getContext(), "Services selected is : " + parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
-          
-          selectedServices = parent.getItemAtPosition(position).toString();
-          worker.setEnabled(false);
+          disableSpinner(worker);
           break;
-        
         case R.id.workerSpinner:
-          //Toast.makeText(parent.getContext(), "Worker selected is : " + parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
-          
-          selectedWorker = parent.getItemAtPosition(position).toString();
-          services.setEnabled(false);
+          disableSpinner(services);
           break;
-        
       }// end of switch
-      
+      getSelectedServices(parent, position);
+
     }// end of else
-    
+
   }// end of onItemSelected
-  
-  public void onNothingSelected(AdapterView<?> arg0) {}
-  
+
+  /**
+   * Sets the background of the drop down menu to white (for both the works and staff)
+   *
+   * by Arnold
+   */
+  public void setBackgroundColor(){
+    services.setPopupBackgroundResource(R.color.servicesSpinnerBackground);
+    worker.setPopupBackgroundResource(R.color.servicesSpinnerBackground);
+  }
+
+  /**
+   * Disables a given spinner
+   * @param currentSpinner-- the spinner to be disabled
+   *
+   * by Arnold
+   */
+  public void disableSpinner(Spinner currentSpinner){
+    currentSpinner.setEnabled(false);
+  }
+
+  /**
+   * Enables both the worker and staff spinner
+   * by Arnold
+   */
+  public void enableSpinners(){
+    services.setEnabled(true);
+    worker.setEnabled(true);
+  }
+
+  /**
+   * Trigged when a view disaperars from the screen or when you have an empty adapter
+   *
+   * @param parent -- it is an adapter view (with no selected item)
+   *
+   * by Arnold
+   */
+  public void onNothingSelected(AdapterView<?> parent) {}
+
+  /**
+   * Gets the selected services
+   *
+   * @param parent -- adapter view of where the selection happens
+   * @param position -- position of the view
+   * @return -- returns the selected item
+   *
+   * by Arnold
+   */
+  public String getSelectedServices(AdapterView<?> parent, int position){
+    return selectedServices = parent.getItemAtPosition(position).toString();
+
+  }
+
   /**
    * Takes user to next Activity - 'Emotions Activity' or, 'Confirmation Activity'
    * @param view - View object - Button object in this scenario
@@ -128,12 +198,12 @@ public class ServicesActivity extends AppCompatActivity implements OnItemSelecte
    *
    */
   public void servicesContinueBtnClicked (View view){
-    
+
     Intent name = new Intent(ServicesActivity.this, EmotionsCheck.class);
     startActivity(name);
     finish();
   } // servicesContinueBtnClicked
-  
+
   /**
    * Starts MainActivity class in 10 seconds
    *
@@ -149,38 +219,7 @@ public class ServicesActivity extends AppCompatActivity implements OnItemSelecte
       }
     }, 10000);
   } // startMainActivity
-  
-  
-  // alvee is working on
-//    @Override
-//    public boolean onTouchEvent(MotionEvent event){
-//        this.mDetector.onTouchEvent(event);
-//        return super.onTouchEvent(event);
-//    }
-//
-//    // alvee is working on
-//    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
-//
-//        @Override
-//        public boolean onSingleTapUp(MotionEvent e) {
-//            Toast.makeText(getApplicationContext(),"Touch detected",Toast.LENGTH_LONG).show();
-//
-//            return super.onSingleTapUp(e);
-//        }
-//    }
 
 
-}
+}// end of file
 
-/**
- * To work on after supper:
- *
- *
- * Setting a black color once the second option has been disabled
- * Setting up an arrow within the spinners
- * Clean up code
- * Modify the timer to go on when screen not touched
- *
- *
- *
- */
