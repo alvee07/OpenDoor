501f90ed7085e657f0379b6d16c03b01f7a56586
Benjamin <bwilson1@ualberta.ca>
2020-01-16 21:23:41 -0700

    added WorkerJSON class and made constructor

---
+++ app/src/main/java/com/example/opendoorapp/WorkerJSON.java
@@ -0,0 +1,11 @@
+package com.example.opendoorapp;
+
+import android.content.Context;
+
+public class WorkerJSON {
+
+  public WorkerJSON(Context context) {
+
+    if(JSON.loadJSON(context, Workers.class, "Benjamin.json").equals())
+  }
+}
