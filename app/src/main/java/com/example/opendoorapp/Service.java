6d8687f4a6f9a260164369fe52801697fbba8038
Benjamin <bwilson1@ualberta.ca>
2020-01-19 16:23:52 -0700

    added service class

---
+++ app/src/main/java/com/example/opendoorapp/Service.java
@@ -0,0 +1,17 @@
+package com.example.opendoorapp;
+
+/**
+ * Service object class. used to store the information of each Service
+ *
+ * Benjamin Wilson
+ */
+public class Service {
+
+  private String departmentName;
+  private String emailList;
+  private boolean isEmotion;
+
+
+  public Service() {
+  }
+}
