2bdf05f92dc0a71ecf303a507c29e6444c2087db
Benjamin <bwilson1@ualberta.ca>
2020-01-19 16:28:14 -0700

    finished service class.

--- app/src/main/java/com/example/opendoorapp/Service.java
+++ app/src/main/java/com/example/opendoorapp/Service.java
@@ -11,7 +11,58 @@ public class Service {
   private String emailList;
   private boolean isEmotion;
 
+  /**
+   * constructor for individual service object
+   * @param departmentName name of the Service/ what they provide
+   * @param emailList list of emails of people who help with service
+   * @param isEmotion boolean on if the user must be checked for emotion
+   */
+  public Service(String departmentName, String emailList, boolean isEmotion) {
+
+    this.departmentName = departmentName;
+    this.emailList = emailList;
+    this.isEmotion = isEmotion;
+
+  }//service constructor
 
   public Service() {
+
+    this.departmentName = "no name";
+    this.emailList = "No email";
+    this.isEmotion = false;
+
+  }//default contructor
+
+  public void setDepartmentName(String departmentName) {
+    this.departmentName = departmentName;
+  }
+
+  public void setEmailList(String emailList) {
+    this.emailList = emailList;
+  }
+
+  public void setEmotion(boolean emotion) {
+    isEmotion = emotion;
+  }
+
+  public String getDepartmentName() {
+    return departmentName;
+  }
+
+  public String getEmailList() {
+    return emailList;
+  }
+
+  public boolean isEmotion() {
+    return isEmotion;
+  }
+
+  @Override
+  public String toString() {
+    return "Service{" +
+            "departmentName='" + departmentName + '\'' +
+            ", emailList='" + emailList + '\'' +
+            ", isEmotion=" + isEmotion +
+            '}';
   }
 }
